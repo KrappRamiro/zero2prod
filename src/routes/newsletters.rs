56c5f186bc92c03c67d19fc4a9e78c818abbe9d9
Ramiro Krapp <krappramiro.jpg@gmail.com>
2026-03-07 15:00:59 -0300

    chapter 10.4

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -202,14 +202,27 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| {
-            PublishError::AuthError(anyhow::anyhow!("Ni puta idea de quien es este username"))
-        })?;
+    let mut user_id = None;
 
-    let current_span = tracing::Span::current();
+    // This is a fake password hash
+    // If the user is found in the database, it replaces the fake hash with the real one.
+    // If the user is not found, it keeps the fake hash.
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, &pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        // THIS CODE ONLY RUNS IF THE USER EXISTS IN THE DB
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
 
     spawn_blocking_with_tracing(move || {
         verify_password_hash(expected_password_hash, credentials.password)
@@ -218,7 +231,12 @@ async fn validate_credentials(
     .context("Failed to spawn blocking task.")
     .map_err(PublishError::UnexpectedError)??;
 
-    Ok(user_id)
+    // This is only set to `Some` if we found credentials in the store
+    // So, even if the default password ends up matching (somehow)
+    // with the provided password,
+    // we never authenticate a non-existing user.
+    // You can easily add a unit test for that precise scenario.
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username or password.")))
 }
 
 /// Function that queries the DB in order to get the crendetials of an username
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -56,10 +56,16 @@ impl TestUser {
         let salt = SaltString::generate(&mut rand::thread_rng());
         // We don't care about the exact Argon2 parameters here
         // given that it's for testing purposes!
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+
+        // Match parameters of the default password
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
 
         sqlx::query!(
             "
@@ -82,7 +88,7 @@ pub struct TestApp {
     pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
-    test_user: TestUser,
+    pub test_user: TestUser,
 }
 
 impl TestApp {
